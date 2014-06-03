3f306c32f15f0ee3350146af0430d1844ac522a7
srmq <srmq@cin.ufpe.br>
2014-06-03 12:22:29 -0300

    initial import

---
+++ FWRDCAImageDatabase/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /home/srmq/cppworkspace/FWRDCA/Debug/libFWRDCA.a
+
+LIBS := -lglpk -larmadillo
+
---
+++ FWRDCAImageDatabase/Debug/src/FWRDCAImageDatabase.d
@@ -0,0 +1,28 @@
+src/FWRDCAImageDatabase.d: ../src/FWRDCAImageDatabase.cpp \
+ /home/srmq/cppworkspace/FWRDCA/src/DissimMatrix.h \
+ /home/srmq/cppworkspace/FWRDCA/src/FuzzyCluster.h \
+ /home/srmq/cppworkspace/FWRDCA/src/FWRDCA.h \
+ /home/srmq/cppworkspace/FWRDCA/src/DissimMatrix.h \
+ /home/srmq/cppworkspace/FWRDCA/src/FuzzyCluster.h \
+ /home/srmq/cppworkspace/FWRDCA/src/FWRDCAGlobal.h \
+ /home/srmq/cppworkspace/FWRDCA/src/FWRDCA.h \
+ /home/srmq/cppworkspace/FWRDCA/src/ConfusionMatrix.h \
+ /home/srmq/cppworkspace/FWRDCA/src/FuzzyIndices.h
+
+/home/srmq/cppworkspace/FWRDCA/src/DissimMatrix.h:
+
+/home/srmq/cppworkspace/FWRDCA/src/FuzzyCluster.h:
+
+/home/srmq/cppworkspace/FWRDCA/src/FWRDCA.h:
+
+/home/srmq/cppworkspace/FWRDCA/src/DissimMatrix.h:
+
+/home/srmq/cppworkspace/FWRDCA/src/FuzzyCluster.h:
+
+/home/srmq/cppworkspace/FWRDCA/src/FWRDCAGlobal.h:
+
+/home/srmq/cppworkspace/FWRDCA/src/FWRDCA.h:
+
+/home/srmq/cppworkspace/FWRDCA/src/ConfusionMatrix.h:
+
+/home/srmq/cppworkspace/FWRDCA/src/FuzzyIndices.h:
